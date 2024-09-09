a03397701c3ce5982ae74f67afae12e787925cb4
Andrew <andrew@andrewholden.xyz>
2024-09-09 12:31:03 -0400

    Added source, gitignore, README, and VSCode files.

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # gravity
 A simple physics simulation to observe the effect of Newtonian gravity on a multi-body simulation
+
+In its current form, it places 3 stationary masses in a 2 x 1 meter space and
+simulates Newtonian gravity to accelerate them toward each other.  The simulation
+takes 1-second steps and runs for 1,000,000 seconds (about 11.5 days) and prints
+the position and velocity of the objects every 10,000 seconds.
+
+Using this, I was able to observe the velocities change direction ober time,
+implying that 3 bodies, placed in an asymetric pattern, is enough to introduce
+angular momentum.
+
+To compile this code, it depends on the [Boost Libraries](https://www.boost.org/),
+specifically the math constants and multiprecision libraries.
---
+++ gravity.cc
@@ -0,0 +1,114 @@
+#include <cmath>
+#include <iostream>
+#include <vector>
+
+#include <boost/math/constants/constants.hpp>
+using boost::math::constants::pi;
+
+#include <boost/multiprecision/cpp_dec_float.hpp>
+using boost::multiprecision::cpp_dec_float_50;
+
+typedef cpp_dec_float_50 Real;
+
+const Real G = 6.67430e-11;  // m^3 kg^-1 s^-2
+const int total_steps = 1000000;
+const int steps_per_print = total_steps / 100;
+
+class Entity
+{
+public:
+    Entity(Real mass, Real x, Real y)
+        : mass(mass), x(x), y(y) {}
+
+    void Update(Real dt)
+    {
+        v_x += a_x * dt;
+        v_y += a_y * dt;
+        x += v_x * dt;
+        y += v_y * dt;
+
+        a_x = 0;
+        a_y = 0;
+    }
+
+    Real const mass;
+    Real x;
+    Real y;
+    Real v_x = 0;
+    Real v_y = 0;
+    Real a_x = 0;
+    Real a_y = 0;
+};
+
+std::ostream &operator<<(std::ostream &os, Entity const &e)
+{
+    auto speed = sqrt(e.v_x * e.v_x + e.v_y * e.v_y);
+    auto angle = atan2(e.v_y, e.v_x) * 180 / pi<Real>();
+    os << 'p' << e.x << ',' << e.y
+        << ", v" << speed << "∠" << angle;
+    return os;
+}
+
+Real Get_attraction(Entity &a, Entity &b)
+{
+    auto dx = b.x - a.x;
+    auto dy = b.y - a.y;
+    auto d2 = dx * dx + dy * dy;
+    auto f = G * a.mass * b.mass / d2;
+    return f;
+}
+
+int main(int argc, char *argv[])
+{
+    std::vector<Entity> entities;
+    entities.push_back(Entity(1, -1, 0));
+    entities.push_back(Entity(1, 1, 0));
+    entities.push_back(Entity(2, 1, 1));
+
+    for(auto &e : entities)
+    {
+        std::cout << e << ' ';
+    }
+    std::cout << std::endl;
+
+    for (int i = 0; i < total_steps; ++i)
+    {
+        for(auto outer = entities.begin(); outer != entities.end(); ++outer)
+        {
+            for(auto inner = entities.begin(); inner != outer; ++inner)
+            {
+                auto force = Get_attraction(*outer, *inner);
+                auto dx = inner->x - outer->x;
+                auto dy = inner->y - outer->y;
+                auto theta = atan2(dy, dx);
+                auto f_x = force * cos(theta);
+                auto f_y = force * sin(theta);
+                outer->a_x += f_x / outer->mass;
+                outer->a_y += f_y / outer->mass;
+                inner->a_x -= f_x / inner->mass;
+                inner->a_y -= f_y / inner->mass;
+            }
+        }
+
+        for(auto &e : entities)
+        {
+            e.Update(0.01);
+        }
+
+        if (i % steps_per_print == 0)
+        {
+            for(auto &e : entities)
+            {
+                std::cout << e << "   ";
+            }
+            std::cout << std::endl;
+        }
+    }
+
+    for(auto &e : entities)
+    {
+        std::cout << e << ' ';
+    }
+    std::cout << std::endl;
+    return 0;
+}
